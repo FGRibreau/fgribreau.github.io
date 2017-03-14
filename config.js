8f10192c6a7e8f4d016ad2e382d0d22c6920b4d7
FG Ribreau <github@fgribreau.com>
2017-03-15 00:24:45 +0100

    🚀

--- config.js
+++ config.js
@@ -24,5 +24,26 @@ module.exports = {
       url: "/img/fgribreau-photo-erdre-nantes.jpg",
       description: "FGRibreau, photo prise à La Chesnaie, le 06/11/2016"
     }
-  ]
+  ],
+  testimonials:[{
+    quote: `François-Guillaume possède à la fois une culture générale informatique très large et une expertise pointue des nouvelles technologies. Ses connaissances théoriques sont illustrées par sa propre expérience terrain, ce qui apporte un éclairage particulièrement intéressant sur les best practices`,
+    who: 'Jin Lei, Hermes Technologies CTO',
+    // when:2017,
+    tags: 'cloud,orchestration,microservices,SOA,scalabilité'.split(',')
+  },{
+    quote: `Suite à un échange d'une demi-journée lié à des problèmes de performances de base de données, les conseils apportés par François-Guillaume nous ont permis de baisser rapidement les temps de réponses de nos services.Il maîtrise son sujet et nous a apporté des conseils sur le court et moyen terme`,
+    who: 'Berger S., Qivivo CTO',
+    // when:2015,
+    tags: 'database,postgresql,performance,optimisation'.split(',')
+  },{
+    quote: `Une intervention efficace et pertinente. Nous avons particulièrement apprécié la capacité de François-Guillaume à comprendre nos enjeux et à proposer une solution adaptée. Ses explications étaient très clair et nous ont énormément aidé à progresser rapidement. Un boost technique adapté à nos besoins`,
+    who: 'Odier V., Kapsuli CTO',
+    // when:2015,
+    tags: 'scalabilité,microservices,SOA'.split(',')
+  },{
+    quote: `L'expertise est un mot souvent utilisé rapidement, ici ce n'était pas dans ce cas. Réponses rapides et efficaces sur les problématiques posées`,
+    who: 'Dudouet P-E., VIF',
+    // when:2015,
+    tags: 'in-memory,kv-store,data-modeling,redis'.split(',')
+  }]
 }
--- src/script/main.js
+++ src/script/main.js
@@ -3,11 +3,19 @@ const config = require('../../config.js');
 import sample from 'lodash/sample';
 import shuffle from 'lodash/shuffle';
 import indexOf from 'lodash/indexOf';
+import noop from 'lodash/noop';
 require('./google-analytics');
 
+function $$(selector, el = document){
+  return Array.from(el.querySelectorAll(selector));
+}
+
+function $(selector){
+  return document.querySelector(selector);
+}
 
 function Gallery(photos) {
-  let cursor = indexOf(photos, sample(photos));
+  let cursor = 3;
 
   function next(options) {
     cursor = cursor + 1 > photos.length -1
@@ -17,36 +25,30 @@ function Gallery(photos) {
     const img = new Image();
     img.onload = () => render(photos[cursor]);
     img.src = photos[cursor].url;
-    setTimeout(next.bind(null, options), options.interval || 10000);
   }
 
   function render({url, description}) {
-    document.querySelector('.header').style.backgroundImage = `url(${url})`;
-    document.querySelector('.footer__copyright').innerText = description;
+    $('.header').style.backgroundImage = `url(${url})`;
+    $('.footer__copyright').innerText = description;
   }
 
-  return {start: next};
+  return {next: next};
 }
-
-const gallery = new Gallery(config.photos);
-gallery.start({interval: 20000});
-
 
 function BackgroundAnimation(el, interval){
   const State = {
-    scale:{min:0.9, max: 1, cur:1, step: 0.1},
-    rotateX:{min:-2, max:2, cur:0, step: 0.5},
-    rotateY:{min:-10, max:10, cur:0, step: 0.5},
-    rotateZ:{min:-2, max:2, cur:0, step: 0.5},
-    translateX:{min:-40, max:40, cur:0, step: 4},
-    translateY:{min:-40, max:40, cur:0, step: 4},
-    posX:{min:0, max:100, cur:0, step: 30},
-    posY:{min:0, max:100, cur:0, step: 30}
+    rotateX:{min:-10, max:10, cur:4, step: 2, render:(x) => `rotateX(${x}deg)`},
+    rotateY:{min:-5, max:5, cur:3, step: 1, render:(x) => `rotateY(${x}deg)`},
+    rotateZ:{min:-2, max:2, cur:0, step: 0.5, render:(x) => `rotateZ(${x}deg)`},
+    translateX:{min:-5, max:5, cur:0, step: 1, render:(x) => `translateX(${x}%)`},
+    translateZ:{min:-30, max:-10, cur:-10, step: 5, render:(x) => `translateZ(${x}vh)`},
   };
 
-  function apply(state){console
-    el.style.transform = `scale(${state.scale.cur}) translateX(${state.translateX.cur}px) translateY(${state.translateY.cur}px) rotateX(${state.rotateX.cur}deg) rotateY(${state.rotateY.cur}deg) rotateZ(${state.rotateZ.cur}deg)`;
-    el.style.backgroundPosition = `background-position: ${state.posX.cur}% ${state.posY.cur}%`;
+  function apply(state){
+    el.style.transform = Object.keys(state).reduce((s, k) => {
+      s += ' ' + state[k].render(state[k].cur);
+      return s;
+    }, '');
   }
 
   apply(State);
@@ -55,18 +57,75 @@ function BackgroundAnimation(el, interval){
     const keys = shuffle(Object.keys(State));
 
     return () => {
-      let i = 3;
-      while(i--){
-        const k = sample(keys);
+      keys.forEach(k => {
         const {min, max, cur, step} = State[k];
-        State[k].cur = Math.max(Math.min(cur + (Math.random() > 0.5 ? 1:-1) * (Math.random()+0.5) * step, max), min);
-      }
+        State[k].cur = Math.max(Math.min(cur + (Math.random() > 0.5 ? 1:-1) * step, max), min);
+        State[k].cur = Math.max(Math.min((Math.random() > 0.5 ? 1:-1) * step, max), min);
+        console.log(k, State[k].cur);
+      })
       apply(State);
-      setTimeout(findNextState, interval);
     };
   })();
 
   setTimeout(findNextState, interval);
+
+  return {
+    findNextState:findNextState
+  };
 }
 
-BackgroundAnimation(document.querySelector('.header'), 1500);
+// Testimonials
+function Testimonials(_els, {onNextTestimonial, onNextPart}){
+  const els = Array.from(_els);
+  let elCursor = -1;
+
+  els.forEach(hide);
+
+  function hide(el){
+    el.classList.add('--hide');
+  }
+
+  function show(el){
+    el.classList.remove('--hide');
+  }
+
+  function nextTestimonial(){
+    onNextTestimonial();
+    elCursor = elCursor+1 > els.length-1 ? 0 : elCursor+1;
+    const el = els[elCursor];
+    show(el);
+
+    const parts = $$('.part', el);
+    parts.forEach(hide);
+
+    function displayNextPart(previousPart){
+      onNextPart();
+      if(previousPart){
+        hide(previousPart);
+      }
+
+      const part = parts.shift();
+      if(!part){
+        hide(el);
+        setTimeout(nextTestimonial, 0);
+        return;
+      }
+
+      show(part);
+      const wait = Math.max(part.innerText.split(' ').length * 0.4 * 1000, 3000);
+      setTimeout(displayNextPart.bind(null, part), wait);
+    }
+
+    displayNextPart();
+  }
+
+  nextTestimonial();
+}
+
+const {next} = Gallery(config.photos);
+const {findNextState} = BackgroundAnimation(document.querySelector('.header'), 1500);
+
+Testimonials($$('.testminials__testimonial'),{
+  onNextPart: findNextState,
+  onNextTestimonial: next,
+});
