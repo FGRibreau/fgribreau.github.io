abdb8a4dd1e1b7c45bd2c4fdfe17ee1115963bd8
FG Ribreau <github@fgribreau.com>
2017-03-21 20:08:10 +0100

    feat(header)

--- blog/webpack.config.js
+++ blog/webpack.config.js
@@ -15,7 +15,14 @@ module.exports = {
       {
         test: /\.scss$/,
         loader: ExtractTextPlugin.extract('css!sass'),
-      }
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+            'file?hash=sha512&digest=hex&name=[hash].[ext]',
+            'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ]
+    }
     ]
   },
   plugins: [new ExtractTextPlugin("style.css", {
